3a926710f33f427d59ed7d54aca26f55b8e06940
Paulina <45172772+Berghauzen@users.noreply.github.com>
2021-05-06 05:55:31 +0200

    Add files via upload

---
+++ moje.js
@@ -0,0 +1,17 @@
+var tabliczkaMnozenia = "<table>";
+
+for (var i = 1; i <= 10; i++)
+{
+    tabliczkaMnozenia += "<tr>";
+
+    for (var j = 1; j <= 10; j++)
+        tabliczkaMnozenia += "<td>" + i * j + "</td>";
+
+    tabliczkaMnozenia += "</tr>";
+}
+
+tabliczkaMnozenia += "</table>";
+
+var rezultat = document.getElementById("rezultat");
+
+rezultat.innerHTML = tabliczkaMnozenia
